9ac7f81091fd2e49543f2e9833250cb92abfc085
desonyuan <42114557@qq.com>
2025-07-03 14:09:26 +0800

    fix

--- components/main/left/LeftBox.tsx
+++ components/main/left/LeftBox.tsx
@@ -3,13 +3,16 @@ import { FC } from "react";
 import DataSourceList from "./module/DataSourceList";
 import VisableChat from "./module/VisableChat";
 
+import { useAppStore } from "@/src/store/app.store";
+
 interface IProps {}
 
 const LeftBox: FC<IProps> = () => {
+  const { situationMode } = useAppStore();
+
   return (
     <div className="absolute h-screen left-0-0 top-0 w-96 px-5 pt-32 flex flex-col gap-10">
-      <DataSourceList />
-      <VisableChat />
+      {situationMode === "simulation" ? <VisableChat /> : <DataSourceList />}
     </div>
   );
 };
